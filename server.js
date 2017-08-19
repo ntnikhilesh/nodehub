a1b94e50693cce49a668063c3490d766cc0da596
nikhil <ntnikhilesh@gmail.com>
2017-08-19 19:32:12 +0530

    3-NodeJS - Express & Morgan - Tutorial 3

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-var http=require('http');
-http.createServer(function (req,res) {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("hello India....\n");
-}).listen(1337,'127.0.0.1');
-console.log("Server running at http://127.0.0.1:1337");
+var express=require('express');
+var app=express();
+var port=process.env.PORT || 8080;
+var morgan=require('morgan'); //middle ware
+
+app.use(morgan('dev')); //inveronment = dev
+
+app.use('/',function (req,res) {
+    res.send('Our first express programme...')
+
+})
+
+app.listen(port)
+
+
+
+//  http=require('http');
+// http.createServer(function (req,res) {
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end("hello India....\n");
+// }).listen(1337,'127.0.0.1');
+console.log("Server running at http://127.0.0.1:8080");
